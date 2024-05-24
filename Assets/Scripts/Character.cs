ab51cad1209fb6bfe31d612c983a351718059d2d
Leejaerin <aaa1967200521@gmail.com>
2024-05-24 16:12:26 +0900

    캐릭터 좌우

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
+    public float Speed = 4f;
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * Speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * Speed * Time.deltaTime);
+        }
+    }
+
+    private void Start()
+    {
+        Animator animator = GetComponent<Animator>();
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+
+}
